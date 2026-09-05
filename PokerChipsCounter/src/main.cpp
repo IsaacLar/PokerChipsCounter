4841bc37e1c586032330f1fa7dd04ed9538388b6
IsaacLar <isaaclarssen@gmail.com>
2026-09-05 21:03:49 +0100

    Added ability for user to enter player names

--- PokerChipsCounter/src/main.cpp
+++ PokerChipsCounter/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <string>
 
 //Initialse LCD object with communication pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -20,6 +21,16 @@ int threeState = 0;
 int fourState = 0;
 
 int numOfPlayers = 2;
+
+//Player struct to hold player name and current balance
+struct Player{
+  char name[4];
+  int balance;
+};
+
+//Null pointer to become array of players
+//VARIABLE DATA TYPE - DELETE BETWEEN ROUNDS
+Player* players = nullptr;
 
 //Function to allow user to enter number of players 
 void enterPlayerNums(){
@@ -63,6 +74,90 @@ void enterPlayerNums(){
       break;
     }
     delay(100);
+  }
+
+  //Define a new Player array of size (whatever the user entered)
+  players = new Player[numOfPlayers];
+
+  //65 = A, 90 = Z
+  //Loop through for each player
+  for (int i = 0; i < numOfPlayers; i++){
+    //Initialise name as AAA
+    int name[3] = {65, 65, 65};
+    //Variable which holds which character is currently being edited - inits at character one
+    int effectedChar = 0;
+    lcd.clear();
+    //LCD OUTPUT:  P{i} Name: AAA
+    lcd.print("P");
+    lcd.print(i+1);
+    lcd.print(" Name: ");
+    lcd.print((char)name[0]);
+    lcd.print((char)name[1]);
+    lcd.print((char)name[2]);
+    lcd.setCursor(9,1);
+    //Cursor to show which letter is being changed
+    lcd.print("-");
+    bool playerNameEntered = false;
+
+    while (!playerNameEntered){
+      oneState = analogRead(pinOne);
+      twoState = analogRead(pinTwo);
+      threeState = analogRead(pinThree);
+      fourState = analogRead(pinFour);
+      //Button 1 = Confirm
+      if (oneState > 1000){
+        playerNameEntered = true;
+      //Button 2 = Increment value
+      } else if (twoState > 1000){
+        //Increment character value
+        name[effectedChar]++;
+        //Loop round if necessary
+        if (name[effectedChar] > 90){
+          name[effectedChar] = 65;
+        }
+        //Update LCD
+        lcd.setCursor(9+effectedChar,0);
+        lcd.print((char)name[effectedChar]);
+
+      //Button 3 = Decrement value
+      } else if (threeState > 1000){
+        //Decrement character value
+        name[effectedChar]--;
+        //Loop round if necessary
+        if (name[effectedChar] < 65){
+          name[effectedChar] = 90;
+        }
+        //Update LCD
+        lcd.setCursor(9+effectedChar, 0);
+        lcd.print((char)name[effectedChar]);
+    
+      //Button 4 = Move right
+      } else if (fourState > 1000){
+        //Clear the current dash
+        lcd.setCursor(9+effectedChar,1);
+        lcd.print(" ");
+        //Increment dash position
+        effectedChar++;
+        //Loop back if necessary
+        if (effectedChar > 2){
+          effectedChar = 0;
+        }
+        //Draw the new dash position
+        lcd.setCursor(9+effectedChar, 1);
+        lcd.print("-");
+      }
+      delay(150);
+    }
+    //Pass the player "name to the struct"
+    for (int j = 0; j < 3; j++){
+      players[i].name[j] = (char)name[j];
+    }
+    //Add null terminator to the end of player name
+    players[i].name[3] = '\0';    
+  }
+  
+  for (int i = 0; i < numOfPlayers; i++){
+    Serial.println(players[i].name);
   }
 }
 
